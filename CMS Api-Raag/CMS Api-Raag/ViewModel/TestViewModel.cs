76a341be5bd18ee7d8e3164a6e37e9911bcd8422
rizvanasherin <risvana.op@experionglobal.com>
2022-02-02 10:03:23 +0530

    Added View Model Folder

---
+++ CMS Api-Raag/CMS Api-Raag/ViewModel/TestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS_Api_Raag.ViewModel
+{
+    public class TestViewModel
+    {
+    }
+}
