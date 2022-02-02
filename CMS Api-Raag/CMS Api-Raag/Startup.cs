6dfb597358fa956a2c61d16a034145c2b5165b80
ganeshanil964 <ganesh.a@experionglobal.com>
2022-02-02 20:37:14 +0530

    all file added

--- CMS Api-Raag/CMS Api-Raag/Startup.cs
+++ CMS Api-Raag/CMS Api-Raag/Startup.cs
@@ -1,5 +1,6 @@
 using CMS_Api_Raag.Models;
 using CMS_Api_Raag.Repository;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -9,11 +10,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.Cors;
 
 namespace CMS_Api_Raag
 {
@@ -30,22 +34,56 @@ namespace CMS_Api_Raag
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-          //  services.AddDbContext<CMSDBContext>(db => db.UseSqlServer(Configuration.GetConnectionString("CMSDBConnection")));
-            services.AddScoped<IDoctorRepository, DoctorRepository>();
-            services.AddControllers().AddNewtonsoftJson(
-              options => {
-                  options.SerializerSettings
-                  .ContractResolver = new DefaultContractResolver();
-              }
-              );
 
-            services
-                .AddControllers().AddNewtonsoftJson(
+            //connection string for datbase inject as dependency 
+            services.AddDbContext<CMSDBContext>(db =>
+            db.UseSqlServer(Configuration.GetConnectionString("CMSDBConnection")));
+
+            //add dependency injection for repository
+            services.AddScoped<IPatientRepository, PatientRepository>();
+            services.AddScoped<IConsultationBillRepository, ConsultationBillRepository>();
+            services.AddScoped<IAppointmentRepository, AppointmentRepository>();
+            services.AddScoped<ITokenRepository, TokenRepository>();
+
+            //register JWT authentication schema
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        //configure authentication scheme with jwt bearer options
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidateLifetime = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                }
+                );
+            services.AddScoped<IDoctorRepository, DoctorRepository>();
+            //Json --resolver
+            services.AddControllers()
+                
+                .AddNewtonsoftJson(Options =>
+                {
+                    Options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                });
+
+            //resolve serialize error and refrence loop
+            services.AddControllers().AddNewtonsoftJson(
                 options =>
                 {
                     options.SerializerSettings.ReferenceLoopHandling =
-                    Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                        Newtonsoft.Json.ReferenceLoopHandling.Ignore;
                 });
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("*", "*", "*");
+
+            //enable Cors
+            services.AddCors();
+
         }
 
 
@@ -53,10 +91,22 @@ namespace CMS_Api_Raag
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
 
+            //Cors settings
+            app.UseCors(Options =>
+                Options.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                );
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+
+
+            //Configure authentication : make authentication available for application
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
 
