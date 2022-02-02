3bc146a0bcea7d26669c13eaf67d98791254ce23
ganeshanil964 <ganesh.a@experionglobal.com>
2022-02-02 20:09:41 +0530

    new file added

--- CMS Api-Raag/CMS Api-Raag/Models/PrescribedMedicine.cs
+++ CMS Api-Raag/CMS Api-Raag/Models/PrescribedMedicine.cs
@@ -10,12 +10,17 @@ namespace CMS_Api_Raag.Models
             PrescriptionDetails = new HashSet<PrescriptionDetails>();
         }
 
+
         public int Pmid { get; set; }
         public int? MedicineId { get; set; }
         public int? DosageId { get; set; }
 
+        public int? Quantity { get; set; }
+
         public virtual Dosage Dosage { get; set; }
         public virtual Medicine Medicine { get; set; }
         public virtual ICollection<PrescriptionDetails> PrescriptionDetails { get; set; }
+
+
     }
 }
--- CMS Api-Raag/CMS Api-Raag/Models/PrescribedTest.cs
+++ CMS Api-Raag/CMS Api-Raag/Models/PrescribedTest.cs
@@ -14,6 +14,8 @@ namespace CMS_Api_Raag.Models
         public int? TestId { get; set; }
         public int? UnitId { get; set; }
 
+        public string NormalRange { get; set; }
+        public decimal Result { get; set; }
         public virtual Test Test { get; set; }
         public virtual TestUnit Unit { get; set; }
         public virtual ICollection<TestDetails> TestDetails { get; set; }
