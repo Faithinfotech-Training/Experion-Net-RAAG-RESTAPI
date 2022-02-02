12a0bbfdfbfe9e2d08976aed8545213b5608a55b
rizvanasherin <risvana.op@experionglobal.com>
2022-02-02 19:11:04 +0530

    Adding new files to controller and view model

---
+++ CMS Api-Raag/CMS Api-Raag/ViewModel/AppointmentViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS_Api_Raag.ViewModel
+{
+    public class AppointmentViewModel
+    {
+        public int AppointmentId { get; set; }
+        public int? PatientId { get; set; }
+        public int DoctorId { get; set; }
+        public int DepId { get; set; }
+        public string DepName { get; set; }
+        public decimal ConsultationFee { get; set; }
+        public DateTime UpdatedDate { get; set; }
+
+    }
+}
