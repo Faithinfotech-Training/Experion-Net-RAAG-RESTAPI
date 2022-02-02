6f910fc9f694b64c3503354db38b0994f93c8d90
ganeshanil964 <ganesh.a@experionglobal.com>
2022-02-02 20:07:38 +0530

    Doctor view model repository dependency injection

--- CMS Api-Raag/CMS Api-Raag/Startup.cs
+++ CMS Api-Raag/CMS Api-Raag/Startup.cs
@@ -1,11 +1,15 @@
+using CMS_Api_Raag.Models;
+using CMS_Api_Raag.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,19 +30,33 @@ namespace CMS_Api_Raag
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+          //  services.AddDbContext<CMSDBContext>(db => db.UseSqlServer(Configuration.GetConnectionString("CMSDBConnection")));
+            services.AddScoped<IDoctorRepository, DoctorRepository>();
+            services.AddControllers().AddNewtonsoftJson(
+              options => {
+                  options.SerializerSettings
+                  .ContractResolver = new DefaultContractResolver();
+              }
+              );
+
+            services
+                .AddControllers().AddNewtonsoftJson(
+                options =>
+                {
+                    options.SerializerSettings.ReferenceLoopHandling =
+                    Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
         }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            // add db injection here
-
 
             app.UseHttpsRedirection();
 
